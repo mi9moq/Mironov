7a1928f778d01db3e99a732c0f2db1501a06538a
Denis Mironov <denis5918@gmail.com>
2024-02-10 15:17:33 +0700

    обработал состояния экрана

--- app/src/main/java/com/mironov/tinkofftesttask/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/mironov/tinkofftesttask/ui/detail/DetailFragment.kt
@@ -9,8 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.mironov.tinkofftesttask.MainActivity
 import com.mironov.tinkofftesttask.databinding.FragmentFilmBinding
+import com.mironov.tinkofftesttask.domain.entity.FilmDetailInfo
 import com.mironov.tinkofftesttask.presentation.ViewModelFactory
+import com.mironov.tinkofftesttask.presentation.detail.DetailState
 import com.mironov.tinkofftesttask.presentation.detail.DetailViewModel
+import com.mironov.tinkofftesttask.ui.utils.collectStateFlow
+import com.squareup.picasso.Picasso
 import javax.inject.Inject
 
 class DetailFragment : Fragment() {
@@ -24,7 +28,10 @@ class DetailFragment : Fragment() {
         }
 
         private const val FILM_ID = "film id"
+        private const val DEFAULT_ID = 1
     }
+
+    private var id = DEFAULT_ID
 
     private val component by lazy {
         (requireActivity() as MainActivity).component
@@ -60,9 +67,70 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addClickListeners()
+        observeViewModel()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
     private fun parseArguments() {
         val args = requireArguments()
-        val id = args.getInt(FILM_ID)
+        id = args.getInt(FILM_ID)
         viewModel.loadFilm(id)
+    }
+
+    private fun addClickListeners() {
+        binding.tryAgain.setOnClickListener {
+            viewModel.loadFilm(id)
+        }
+    }
+
+    private fun observeViewModel() {
+        collectStateFlow(viewModel.state, ::applyState)
+    }
+
+    private fun applyState(state: DetailState) {
+
+        when (state) {
+            DetailState.Initial -> Unit
+            is DetailState.Content -> applyContentState(state.content)
+            DetailState.Error -> applyErrorState()
+            DetailState.Loading -> applyLoadingState()
+        }
+    }
+
+    private fun applyContentState(film: FilmDetailInfo) {
+
+        with(binding) {
+            content.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE
+            error.visibility = View.GONE
+            Picasso.get().load(film.posterUrl).into(poster)
+            name.text = film.name
+            genres.text = film.genres.joinToString(", ")
+            countries.text = film.countries.joinToString(", ")
+            description.text = film.description
+        }
+    }
+
+    private fun applyErrorState() {
+        with(binding) {
+            content.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE
+            error.visibility = View.GONE
+        }
+    }
+
+    private fun applyLoadingState() {
+        with(binding) {
+            content.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE
+            error.visibility = View.GONE
+        }
     }
 }
