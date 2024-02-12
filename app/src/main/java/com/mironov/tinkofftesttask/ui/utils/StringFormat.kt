cec3ef3a09cd73408053029d5ee1a324c52fb172
Denis Mironov <denis5918@gmail.com>
2024-02-12 12:09:54 +0700

    сделал отображение жанра с большой буквы

--- app/src/main/java/com/mironov/tinkofftesttask/ui/utils/StringFormat.kt
+++ app/src/main/java/com/mironov/tinkofftesttask/ui/utils/StringFormat.kt
@@ -1,4 +1,8 @@
 package com.mironov.tinkofftesttask.ui.utils
 
-fun formatFilmDescription(genre: String, year: Int): String =
-    "$genre ($year)"
+fun formatFilmDescription(genre: String, year: Int): String {
+    val genreTitleCase = genre.replaceFirstChar {
+        it.titlecase()
+    }
+    return "$genreTitleCase ($year)"
+}
